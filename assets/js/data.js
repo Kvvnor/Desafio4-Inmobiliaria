ce0fd3deb1338425031aae0c720d46db1bffefb0
Kevin Ortiz <kvvinortiz@gmail.com>
2025-05-18 18:29:36 -0400

    Integración de data de propiedades en alquiler

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,5 +1,5 @@
 const propiedades_venta = [
-    
+
   {
     nombre: 'Casa en 2224 Weona Dr',
     src: 'https://www.zillow.com/homedetails/2224-Weona-Dr-Nashville-TN-37214/41341562_zpid/',
@@ -19,7 +19,7 @@ const propiedades_venta = [
     habitaciones: 3,
     banos: 4,
     costo: 765000,
-    smoke: false,
+    smoke: true,
     pets: true
   },
   {
@@ -41,8 +41,8 @@ const propiedades_venta = [
     habitaciones: 2,
     banos: 4,
     costo: 690000,
-    smoke: false,
-    pets: true
+    smoke: true,
+    pets: false
   },
   {
     nombre: 'Residencia en 4508 Belmont Park Ter',
@@ -52,6 +52,65 @@ const propiedades_venta = [
     habitaciones: 5,
     banos: 4,
     costo: 1450000,
+    smoke: false,
+    pets: true
+  }
+];
+
+
+const propiedades_alquiler = [
+  {
+    nombre: 'Apartamento en Gloria Park Villas',
+    src: 'https://www.forrent.com/es/find/NV/metro-Las%2BVegas/Las%2BVegas',
+    descripcion: 'Apartamento moderno con acceso a gimnasio, piscina y áreas comunes.',
+    ubicacion: 'Las Vegas, NV',
+    habitaciones: 2,
+    banos: 4,
+    costo: 1800,
+    smoke: true,
+    pets: true
+  },
+  {
+    nombre: 'Condominio en 575 S Royal Crest Cir',
+    src: 'https://www.forrent.com/es/find/NV/metro-Las%2BVegas/Las%2BVegas',
+    descripcion: 'Condominio de 1 habitación con cocina equipada y balcón privado.',
+    ubicacion: 'Las Vegas, NV',
+    habitaciones: 1,
+    banos: 4,
+    costo: 1000,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: 'Apartamento en Sierra Park',
+    src: 'https://www.forrent.com/es/find/NV/metro-Las%2BVegas/Las%2BVegas',
+    descripcion: 'Estudio con acabados modernos, cerca de tiendas y restaurantes.',
+    ubicacion: 'Las Vegas, NV',
+    habitaciones: 1,
+    banos: 4,
+    costo: 845,
+    smoke: true,
+    pets: false
+  },
+  {
+    nombre: 'Casa en 11835 Corenzio Ave',
+    src: 'https://espanol.century21.com/propiedades-en-venta/nevada/LSNV/en-alquiler/',
+    descripcion: 'Casa espaciosa de 4 habitaciones con jardín y garaje para dos autos.',
+    ubicacion: 'Las Vegas, NV',
+    habitaciones: 4,
+    banos: 4,
+    costo: 3300,
+    smoke: false,
+    pets: true
+  },
+  {
+    nombre: 'Apartamento en The Allister',
+    src: 'https://www.forrent.com/es/find/NV/metro-Las%2BVegas/Las%2BVegas',
+    descripcion: 'Apartamento de lujo con acceso a piscina, gimnasio y áreas recreativas.',
+    ubicacion: 'Las Vegas, NV',
+    habitaciones: 3,
+    banos: 4,
+    costo: 1840,
     smoke: false,
     pets: true
   }
