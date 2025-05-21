a1eb6c8176aa314934b7cc85f099f27d01dba1a8
Kevin Ortiz <kvvinortiz@gmail.com>
2025-05-21 13:44:50 -0400

    Actualización de imágenes y datos

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,18 +2,18 @@ const propiedades_venta = [
 
   {
     nombre: 'Casa en 2224 Weona Dr',
-    src: 'https://www.zillow.com/homedetails/2224-Weona-Dr-Nashville-TN-37214/41341562_zpid/',
+    src: 'https://photos.zillowstatic.com/fp/0f3c5a9827740289f4c01693bdb0d601-cc_ft_960.jpg',
     descripcion: 'Casa remodelada con patio, cocina moderna y excelente ubicación cerca del aeropuerto.',
     ubicacion: 'Nashville, TN 37214',
-    habitaciones: 3,
-    banos: 4,
+    habitaciones: 2,
+    banos: 2,
     costo: 479000,
     smoke: false,
     pets: true
   },
   {
     nombre: 'Casa moderna en 862 W Sharpe Ave',
-    src: 'https://www.zillow.com/homedetails/862-W-Sharpe-Ave-Nashville-TN-37206/2067844376_zpid/',
+    src: 'https://photos.zillowstatic.com/fp/42b60212f0c15d015c18119b04a84b2d-cc_ft_960.webp',
     descripcion: 'Elegante casa nueva con acabados de lujo, terraza en la azotea y garaje para dos autos.',
     ubicacion: 'East Nashville, Nashville, TN',
     habitaciones: 3,
@@ -24,18 +24,18 @@ const propiedades_venta = [
   },
   {
     nombre: 'Residencia en 1125 Calvin Ave',
-    src: 'https://www.redfin.com/TN/Nashville/1125-Calvin-Ave-37206/home/114370203',
+    src: 'https://photos.zillowstatic.com/fp/81e6d31df3899339210829d098938690-cc_ft_960.webp',
     descripcion: 'Hermosa casa familiar de 4 dormitorios con amplio jardín, ideal para mascotas.',
     ubicacion: 'Inglewood, Nashville, TN',
     habitaciones: 4,
-    banos: 4,
+    banos: 2,
     costo: 849000,
     smoke: false,
     pets: true
   },
   {
     nombre: 'Casa en 1408 Pillow St',
-    src: 'https://www.redfin.com/TN/Nashville/1408-Pillow-St-37203/home/114247098',
+    src: 'https://photos.zillowstatic.com/fp/0f3c5a9827740289f4c01693bdb0d601-cc_ft_960.jpg',
     descripcion: 'Moderna casa en The Gulch con techos altos, diseño abierto y excelente conectividad.',
     ubicacion: 'The Gulch, Nashville, TN',
     habitaciones: 2,
@@ -46,11 +46,11 @@ const propiedades_venta = [
   },
   {
     nombre: 'Residencia en 4508 Belmont Park Ter',
-    src: 'https://www.zillow.com/homedetails/4508-Belmont-Park-Ter-Nashville-TN-37215/41336469_zpid/',
+    src: 'https://photos.zillowstatic.com/fp/0f3c5a9827740289f4c01693bdb0d601-cc_ft_960.jpg',
     descripcion: 'Lujosa propiedad en Green Hills con cinco habitaciones, jardín privado y acabados premium.',
     ubicacion: 'Green Hills, Nashville, TN',
-    habitaciones: 5,
-    banos: 4,
+    habitaciones: 3,
+    banos: 3,
     costo: 1450000,
     smoke: false,
     pets: true
@@ -58,43 +58,43 @@ const propiedades_venta = [
 ];
 
 
-const propiedades_alquiler = [
+const propiedades_arriendo = [
   {
     nombre: 'Apartamento en Gloria Park Villas',
-    src: 'https://www.forrent.com/es/find/NV/metro-Las%2BVegas/Las%2BVegas',
+    src: 'https://images1.forrent.com/i2/1kf6McSD9qWWLwGNo9oHd2jvz9XsMSxazP-OE6sek6U/117/image.jpg?p=1',
     descripcion: 'Apartamento moderno con acceso a gimnasio, piscina y áreas comunes.',
     ubicacion: 'Las Vegas, NV',
-    habitaciones: 2,
-    banos: 4,
+    habitaciones: 3,
+    banos: 3,
     costo: 1800,
     smoke: true,
     pets: true
   },
   {
     nombre: 'Condominio en 575 S Royal Crest Cir',
-    src: 'https://www.forrent.com/es/find/NV/metro-Las%2BVegas/Las%2BVegas',
+    src: 'https://ssl.cdn-redfin.com/photo/95/bigphoto/382/2495382_0.jpg',
     descripcion: 'Condominio de 1 habitación con cocina equipada y balcón privado.',
     ubicacion: 'Las Vegas, NV',
     habitaciones: 1,
-    banos: 4,
+    banos: 1,
     costo: 1000,
     smoke: false,
     pets: true
   },
   {
     nombre: 'Apartamento en Sierra Park',
-    src: 'https://www.forrent.com/es/find/NV/metro-Las%2BVegas/Las%2BVegas',
+    src: 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/622900149.jpg?k=5c4a5131bb83dff9c9d55ffffbd1549fb2cccee6b27e8d7f485cb3b6a558e091&o=',
     descripcion: 'Estudio con acabados modernos, cerca de tiendas y restaurantes.',
     ubicacion: 'Las Vegas, NV',
-    habitaciones: 1,
-    banos: 4,
+    habitaciones: 2,
+    banos: 2,
     costo: 845,
     smoke: true,
     pets: false
   },
   {
     nombre: 'Casa en 11835 Corenzio Ave',
-    src: 'https://espanol.century21.com/propiedades-en-venta/nevada/LSNV/en-alquiler/',
+    src: 'https://photos.zillowstatic.com/fp/0f3c5a9827740289f4c01693bdb0d601-cc_ft_960.jpg',
     descripcion: 'Casa espaciosa de 4 habitaciones con jardín y garaje para dos autos.',
     ubicacion: 'Las Vegas, NV',
     habitaciones: 4,
@@ -105,11 +105,11 @@ const propiedades_alquiler = [
   },
   {
     nombre: 'Apartamento en The Allister',
-    src: 'https://www.forrent.com/es/find/NV/metro-Las%2BVegas/Las%2BVegas',
+    src: 'https://photos.zillowstatic.com/fp/0f3c5a9827740289f4c01693bdb0d601-cc_ft_960.jpg',
     descripcion: 'Apartamento de lujo con acceso a piscina, gimnasio y áreas recreativas.',
     ubicacion: 'Las Vegas, NV',
-    habitaciones: 3,
-    banos: 4,
+    habitaciones: 2,
+    banos: 1,
     costo: 1840,
     smoke: false,
     pets: true
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const venta = document.querySelector("#venta");
-const rowVenta = document.querySelector(".row");
+const rowVenta = venta.querySelector(".row");
 
 
 propiedades_venta.forEach( (venta, index) => {
@@ -7,7 +7,7 @@ propiedades_venta.forEach( (venta, index) => {
     if(index < 3) {
 
         rowVenta.innerHTML += `
-    <div class="col-md-4 mb-4">
+     <div class="col-md-4 mb-4">
             <div class="card">
               <img
                 src="${venta.src}"
@@ -49,15 +49,70 @@ propiedades_venta.forEach( (venta, index) => {
 
               </div>
             </div>
-          </div>
+       </div>
     `;
     }
 
 });
 
-propiedades_alquiler.forEach( (alquiler, index) => {
 
 
+
+const arriendo = document.querySelector("#alquiler");
+const rowArriendo = arriendo.querySelector(".row");
+
+propiedades_arriendo.forEach( (arriendo, index) => {
+
+  console.log(index);
+
+    if(index < 3) {
+
+        rowArriendo.innerHTML += `
+     <div class="col-md-4 mb-4">
+            <div class="card">
+              <img
+                src="${arriendo.src}"
+                class="card-img-top"
+                alt="Imagen del departamento"
+              />
+              <div class="card-body">
+                <h5 class="card-title">
+                  ${arriendo.nombre}
+                </h5>
+                <p class="card-text">
+                  ${arriendo.descripcion}
+                </p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i> ${arriendo.ubicacion}
+                </p>
+                <p>
+                  <i class="fas fa-bed"></i> ${arriendo.habitaciones} Habitaciones |
+                  <i class="fas fa-bath"></i> ${arriendo.banos} Baños
+                </p>
+                <p><i class="fas fa-dollar-sign"></i> ${arriendo.costo} </p>
+                <p class="${arriendo.smoke ? "text-success" : "text-danger"}">
+
+                ${
+                    arriendo.smoke
+                        ? `<i class="fa-solid fa-check"></i> Se permite fumar`
+                        : `<i class="fa-solid fa-ban"></i> No se permite fumar`
+                }
+                </p>
+
+                <p class="${arriendo.pets ? "text-success" : "text-danger"}">
+
+                ${
+                    arriendo.pets
+                        ? `<i class="fa-solid fa-check"></i> Se permiten mascotas`
+                        : `<i class="fa-solid fa-ban"></i> No se permiten mascotas`
+                }
+                </p>
+
+              </div>
+            </div>
+       </div>
+    `;
+    }
 
     
 });
